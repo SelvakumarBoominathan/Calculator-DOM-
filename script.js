2b23e3805d2d9ec64ee76f7197c666e9cf2b09ae
Selvakumar Boominathan <selvans2k@gmail.com>
2024-02-13 20:07:42 +0530

    work progress added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // container creation
 const container = document.createElement('div');
 container.classList.add('container');
+document.body.appendChild(container);
 
 // display creation
 const displayInput = document.createElement('input');
@@ -12,5 +13,30 @@ container.appendChild(displayInput);
 const buttonsDiv = document.createElement('div');
 buttonsDiv.classList.add('buttons');
 container.appendChild(buttonsDiv);
+
+const buttonValues = [
+  ['C', '←', '.', '*'],
+  ['7', '8', '9', '/'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '00', '=']
+];
+
+
+buttonValues.forEach(rowvalue => {
+  const row = document.createElement('div');
+  row.classList.add('row-Data');
+  rowvalue.forEach(value => {
+    const buttons = document.createElement('button');
+    buttons.textContent = value;
+    buttons.setAttribute('data-value', value);
+    row.appendChild(buttons);
+  });
+  buttonsDiv.appendChild(row);
+});
+
+
+
+
 
 

b7a8e47bfa8615b19d4a3c4d6a3b6d084febd85e
Selvakumar Boominathan <selvans2k@gmail.com>
2024-02-17 00:23:06 +0530

    Final modification done and updated

--- script.js
+++ script.js
@@ -1,75 +1,27 @@
-// container creation
-const container = document.createElement('div');
-container.classList.add('container');
-document.body.appendChild(container);
-
-// display creation
-const displayInput = document.createElement('input');
-displayInput.setAttribute('type', 'text');
-displayInput.classList.add('display');
-container.appendChild(displayInput);
-
-//Create buttons div
-const buttonsDiv = document.createElement('div');
-buttonsDiv.classList.add('buttons');
-container.appendChild(buttonsDiv);
-
-const buttonValues = [
-  ['C', '←', '.', '*'],
-  ['7', '8', '9', '/'],
-  ['4', '5', '6', '-'],
-  ['1', '2', '3', '+'],
-  ['0', '00', '=', '🐼']
-];
-
-
-buttonValues.forEach(rowvalue => {
-  const row = document.createElement('div');
-  row.classList.add('row-Data');
-  rowvalue.forEach(value => {
-    const buttons = document.createElement('button');
-    buttons.textContent = value;
-    buttons.setAttribute('data-value', value);
-    buttons.classList.add('button')
-    row.appendChild(buttons);
-  });
-  buttonsDiv.appendChild(row);
-});
-
-
-//EVENT LISTENERS
+const displayInput = document.querySelector('#result');
+const buttonsDiv = document.querySelector('.buttons');
 
 buttonsDiv.addEventListener('click', event => {
   const target = event.target;
   const value = target.getAttribute('data-value');
 
-  // Check if the clicked element is a button
   if (target.tagName === 'BUTTON' && value) {
-    // Handle button click based on its value
     switch (value) {
       case 'C':
-        // Clear the display
         displayInput.value = '';
         break;
       case '←':
-        // Remove the last character from the display
         displayInput.value = displayInput.value.slice(0, -1);
         break;
       case '=':
-        // Evaluate the expression and display the result
         try {
           displayInput.value = eval(displayInput.value);
         } catch (error) {
-          // Handle any errors
           displayInput.value = 'Error';
         }
         break;
       default:
-        // Append the clicked button value to the display
         displayInput.value += value;
     }
   }
 });
-
-
-
